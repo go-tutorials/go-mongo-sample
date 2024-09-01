89e39381cfdb4e60f017499d55c0641ed0394fc9
Duc Nguyen <minhduc140583@gmail.com>
2024-09-01 23:55:38 +0700

    Separate query into new package

--- internal/user/repository/adapter/adapter.go
+++ internal/user/repository/adapter/adapter.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
@@ -129,35 +128,4 @@ func (r *UserAdapter) Search(ctx context.Context, filter *model.UserFilter, limi
 	}
 	err = cursor.All(ctx, &users)
 	return users, total, err
-}
-
-func BuildQuery(filter *model.UserFilter) (bson.D, bson.M) {
-	query := bson.D{}
-	if len(filter.Id) > 0 {
-		query = append(query, bson.E{Key: "_id", Value: filter.Id})
-	}
-	if filter.DateOfBirth != nil {
-		dobFilter := bson.M{}
-		if filter.DateOfBirth.Min != nil {
-			dobFilter["$gte"] = filter.DateOfBirth.Min
-		}
-		if filter.DateOfBirth.Max != nil {
-			dobFilter["$lte"] = filter.DateOfBirth.Max
-		}
-		if len(dobFilter) > 0 {
-			query = append(query, bson.E{Key: "dateOfBirth", Value: dobFilter})
-		}
-	}
-	if len(filter.Username) > 0 {
-		query = append(query, bson.E{Key: "username", Value: primitive.Regex{Pattern: fmt.Sprintf("^%v", filter.Username), Options: "i"}})
-	}
-	if len(filter.Email) > 0 {
-		query = append(query, bson.E{Key: "email", Value: primitive.Regex{Pattern: fmt.Sprintf("^%v", filter.Email), Options: "i"}})
-	}
-	if len(filter.Phone) > 0 {
-		query = append(query, bson.E{Key: "phone", Value: primitive.Regex{Pattern: fmt.Sprintf("\\w*%v\\w*", filter.Phone), Options: "i"}})
-	}
-
-	fields := mgo.GetFields(filter.Fields, reflect.TypeOf(model.User{}))
-	return query, fields
 }
